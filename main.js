e40367045957cc14522a1ff0104ab3e6656926a1
Leonardo <leonardo.bongianni@gmail.com>
2021-03-23 16:10:48 +0100

    Esercizio Completo
    Bonus eseguito

---
+++ main.js
@@ -0,0 +1,16 @@
+// Descrizione
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+var app = new Vue (
+  {
+    el: "#root",
+    data: {
+      messaggio: "Ciao Mondo!",
+      image: "./assets/img/mappamondo.jpg",
+      myclass: "txt-center",
+      link: "./assets/img/donut.png",
+      video: "https://www.youtube.com/watch?v=SQgyyqOBf04&ab_channel=MantonioBanderas"
+    }
+  }
+);
